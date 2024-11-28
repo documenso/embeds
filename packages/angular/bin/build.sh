2d8abf7f48defd77a1b5cb5c9b2e8e0cbba37d61
Mythie <me@lucasjamessmith.me>
2024-11-28 14:35:20 +1100

    fix: update build script to work on linux

--- packages/angular/bin/build.sh
+++ packages/angular/bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname "$0")/.."
 
 # Replace or copy ./util/trusted-resource-url-pipe.ts to ./src/trusted-resource-url-pipe.ts 
@@ -8,20 +10,27 @@ cp ./util/trusted-resource-url-pipe.ts ./src/trusted-resource-url-pipe.ts
 # Array of files to process
 files=('./src/direct-template.ts' './src/sign-document.ts')
 
+# Detect OS type
+OS_TYPE=$(uname)
+
 # Loop over the array of files
 for file in "${files[@]}"
 do
-    # Update the iframe line
-    sed -i '' 's/<iframe #__iframe \[class\]="className" \[attr\.src\]="src"><\/iframe>/<iframe #__iframe \[class\]="className" \[attr\.src\]="src | trustedResourceUrl"><\/iframe>/' "$file"
-
-    # Update the imports line
-    sed -i '' 's/imports: \[CommonModule\],/imports: \[CommonModule, TrustedResourceUrlPipe\],/' "$file"
-
-    # Add the import statement at the top of the file
-    sed -i '' '1i\
+    if [ "$OS_TYPE" = "Darwin" ]; then
+        # macOS (BSD) sed commands
+        sed -i '' 's/<iframe #__iframe \[class\]="className" \[attr\.src\]="src"><\/iframe>/<iframe #__iframe \[class\]="className" \[attr\.src\]="src | trustedResourceUrl"><\/iframe>/' "$file"
+        sed -i '' 's/imports: \[CommonModule\],/imports: \[CommonModule, TrustedResourceUrlPipe\],/' "$file"
+        sed -i '' '1i\
 import { TrustedResourceUrlPipe } from "./trusted-resource-url-pipe";
 ' "$file"
-
+    else
+        # GNU sed commands (Linux and others)
+        sed -i 's/<iframe #__iframe \[class\]="className" \[attr\.src\]="src"><\/iframe>/<iframe #__iframe \[class\]="className" \[attr\.src\]="src | trustedResourceUrl"><\/iframe>/' "$file"
+        sed -i 's/imports: \[CommonModule\],/imports: \[CommonModule, TrustedResourceUrlPipe\],/' "$file"
+        sed -i '1i\
+import { TrustedResourceUrlPipe } from "./trusted-resource-url-pipe";
+' "$file"
+    fi
 done
 
 ng build
